9edf799ad6386dd6f7c7a055f91a0a9d2c5f3f3c
eric.rao <eric.rao@ringcentral.com>
2020-07-26 22:50:57 +0800

    declare as text.

--- model/question.go
+++ model/question.go
@@ -7,13 +7,13 @@ import (
 type Option struct {
 	gorm.Model
 	Order uint
-	Description string
+	Description string `gorm:"type:text"`
 }
 
 type Question struct {
 	gorm.Model
-	Topic    string   `json:topic`
+	Topic    string   `gorm:"type:text" json:topic`
 	Options  []Option `json:"options"`
 	Answer   uint      `json:"answer"`
-	Analysis string   `json:"analysis"`
+	Analysis string   `gorm:"type:text" json:"analysis"`
 }
