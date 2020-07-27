211d5954637ec24b475ab9a94251f6ab13374d76
eric.rao <eric.rao@ringcentral.com>
2020-07-27 21:03:47 +0800

    use varchar instead of text.

--- model/question.go
+++ model/question.go
@@ -12,8 +12,8 @@ type Option struct {
 
 type Question struct {
 	gorm.Model
-	Topic    string   `gorm:"type:text" json:topic`
+	Topic    string   `gorm:"type:varchar(1023)" json:topic`
 	Options  []Option `json:"options"`
 	Answer   uint      `json:"answer"`
-	Analysis string   `gorm:"type:text" json:"analysis"`
+	Analysis string   `gorm:"type:varchar(2047)" json:"analysis"`
 }
